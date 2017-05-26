a598e7df2a374896f190c43ae9b70f1a64122777
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2017-05-25 20:12:49 -0400

    UNSAFE

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate smallvec;
 use num_traits::FromPrimitive;
 
 const ENTRY_BITS: usize = 2;
+const ENTRY_MASK: u32 = !(!0u32 << ENTRY_BITS);
 const BOARD_WORD_BITS: usize = 32;
 const BOARD_WORD_ENTRIES: usize = BOARD_WORD_BITS / ENTRY_BITS;
 const BOARD_WORDS: usize = 16;
@@ -30,26 +31,37 @@ impl Board {
         Board { size: size as u32, data: [0; BOARD_WORDS] }
     }
 
-    pub fn set(mut self, x: usize, y: usize, v: Entry) -> Board {
+    pub fn set(self, x: usize, y: usize, v: Entry) -> Board {
         assert!(x < self.size as usize);
         assert!(y < self.size as usize);
+        unsafe { self.set_unchecked(x, y, v as u32) }
+    }
+
+    pub unsafe fn set_unchecked(mut self, x: usize, y: usize, v: u32) -> Board {
         let i = x * (self.size as usize) + y;
         let j = i / BOARD_WORD_ENTRIES;
         let k = ENTRY_BITS * (i % BOARD_WORD_ENTRIES);
         let v = (v as u32) << k;
-        let m = !(!(!0 << ENTRY_BITS) << k);
-        self.data[j] = (self.data[j] & m) | v;
+        let m = !(ENTRY_MASK << k);
+        {
+            let e = self.data.get_unchecked_mut(j);
+            *e = (*e & m) | v;
+        }
         self
     }
 
     pub fn get(&self, x: usize, y: usize) -> Entry {
         assert!(x < self.size as usize);
         assert!(y < self.size as usize);
-        let i = x * (self.size as usize) + y;
-        let v = self.data[i / BOARD_WORD_ENTRIES];
-        let v = v >> (ENTRY_BITS * (i % BOARD_WORD_ENTRIES));
-        let v = v & !(!0 << ENTRY_BITS);
+        let v = unsafe { self.get_unchecked(x, y) };
         Entry::from_u32(v).unwrap()
+    }
+
+    pub unsafe fn get_unchecked(&self, x: usize, y: usize) -> u32 {
+        let i = x * (self.size as usize) + y;
+        let v = self.data.get_unchecked(i / BOARD_WORD_ENTRIES);
+        let v = v >> (ENTRY_BITS * (i % BOARD_WORD_ENTRIES));
+        v & ENTRY_MASK
     }
 }
 
@@ -68,5 +80,8 @@ mod tests {
         assert_eq!(Entry::Empty, b.get(5, 6));
         assert_eq!(Entry::Empty, b.get(6, 8));
         assert_eq!(Entry::Player1, b.get(5, 7));
+        let b1 = b.set(5, 7, Entry::Block);
+        assert_eq!(Entry::Player1, b.get(5, 7));
+        assert_eq!(Entry::Block, b1.get(5, 7));
     }
 }
